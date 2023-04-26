b6df98703a8af43a0f1fbfcc25adde9d7d8dd9b6
ALANNE WITHNEY <128842001+WITHNEY01@users.noreply.github.com>
2023-04-26 17:19:34 -0300

    Add files via upload

---
+++ Q2/CalculoE.java
@@ -0,0 +1,23 @@
+package Q2;
+import java.util.Scanner;
+
+public class CalculoE {
+
+		    public static void main(String[] args) {
+		        Scanner sc = new Scanner(System.in);
+		        int n;
+
+		        System.out.print("Digite um número inteiro e positivo: ");
+		        n = sc.nextInt();
+
+		        double e = 1.0;
+		        double fatorial = 1.0;
+
+		        for (int i = 1; i <= n; i++) {
+		            fatorial *= i;
+		            e += 1.0 / fatorial;
+		        }
+
+		        System.out.println("O valor de E é: " + e);
+		    }
+		}
